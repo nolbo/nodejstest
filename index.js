3a8dd2e857b57d9f9d8d26d4b86e14efe25c0a65
nolbo <jun9975sun@gmail.com>
2021-02-11 12:24:24 +0900

    bot12

--- index.js
+++ index.js
@@ -3,22 +3,34 @@ const Discord = require('discord.js');
 const client  = new Discord.Client();
 let power = true;
 
-client.once('ready', () => {
-    console.log('Login!');
-});
+try {
+    client.once('ready', () => {
+        console.log('Login!');
+    });
 
-client.on('message', msg => {
-    try {
+    client.on('message', msg => {
         let cmd = msg.content.split(' ')[0];
         let val = msg.content.replace(`${cmd} `, '');
 
-        if(power === true && msg.author.tag !== 'Vson#9403' && msg.author.tag === 'nolbo#4403' && cmd === ';;'){
-            msg.channel.send("```JavaScript\n" + eval(val) + "```");
+        if(msg.author.tag !== 'Vson#9403' && msg.author.tag === 'nolbo#4403' && cmd === 'on') {
+            power = true;
+            msg.channel.send('`On`');
         }
-    } catch(e) {
+
+        else if(power === true && msg.author.tag !== 'Vson#9403' && msg.author.tag === 'nolbo#4403' && cmd === ';;') {
+            try {
+                msg.channel.send("```JavaScript\n" + eval(val) + "```");
+            } catch(e) {
+                msg.channel.send(e);
+            }
+        }
+    });
+} catch(e) {
+    client.on('messgae', msg => {
         msg.channel.send(e);
         power = false;
-    }
-});
+    });
+}
+
 
 client.login(process.env.TOKEN);
