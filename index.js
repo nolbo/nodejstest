291243c90719a41ce1a29aa99b5c420e9520dba8
nolbo <jun9975sun@gmail.com>
2021-02-11 20:25:27 +0900

    bot16

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 
 const client  = new Discord.Client();
-let power = true;
+let power     = true;
+let runtime   = false;
 
 try {
     client.once('ready', () => {
@@ -12,14 +13,21 @@ try {
         let cmd = msg.content.split(' ')[0];
         let val = msg.content.replace(`${cmd} `, '');
 
-        if(power === false && msg.author.tag !== 'Vson#9403' && msg.author.tag === 'nolbo#4403' && msg.content === 'on') {
+        if(power === false && msg.author.tag !== 'Vson#9403' && msg.author.tag === 'nolbo#4403' && msg.content === ';power') {
             power = true;
-            msg.channel.send('`On`');
+            msg.channel.send('`On: Power`');
+        }
+
+        else if(power === false && msg.author.tag !== 'Vson#9403' && msg.author.tag === 'nolbo#4403' && msg.content === ';runtime') {
+            runtime = true;
+            msg.channel.send('`On: Runtime`');
         }
 
         else if(power === true && msg.author.tag !== 'Vson#9403' && msg.author.tag === 'nolbo#4403' && cmd === ';;') {
             try {
+                let start = new Date().getTime();
                 msg.channel.send("```JavaScript\n" + eval(val) + "```");
+                if(runtime === true) msg.channel.send('`Runtime: ' + new Date().getTime() - start + 'ms`');
             } catch(e) {
                 msg.channel.send("```\n" + String(e) + "```");
             }
